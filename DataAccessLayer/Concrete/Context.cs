149d605bb43b732c330a0b2f97f7f62b101d0a3c
Melih Dinçer <m_dincer41@hotmail.com>
2023-02-23 21:57:14 +0300

    Dbset tanımlamaları yapıldı.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,21 @@ namespace DataAccessLayer.Concrete
 {
     public class Context:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.onconfiguring(optionsbuilder);
+            optionsBuilder.UseSqlServer("server=MELIH;database=CoreProjeDB;integrated security=true");
+        }
+
+        public DbSet<About> Abouts { get; set; } // <About> == Sınıfın ismi || Abouts  SQL Server da gözükecek tablo adı.
+        public DbSet<Context> Contexts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }
 }
