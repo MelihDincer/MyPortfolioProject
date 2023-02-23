6bb7102c219453a91c2614ca7ba9b519b0c4c855
Melih Dinçer <m_dincer41@hotmail.com>
2023-02-23 21:17:32 +0300

    Entity katmanı oluşturulup, entity ler tanımlandı.

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Contact // iletişim
+    {
+        [Key]
+        public int ContactID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature  // özellik
+    {
+        [Key]
+        public int FeatureID { get; set; }
+        public string Header { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+}
