6f499628effbfa4bdc68d0e8eba930f4a4592e71
Melih Dinçer <m_dincer41@hotmail.com>
2023-02-25 17:46:38 +0300

    Generic interface oluşturuldu.

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    //Ortak metotlar burada oluşturulan genericdal interface den kalıtım alınacak. Base interface diyebiliriz.
+    public interface IGenericDal<T> where T : class // T ye bir sınıf gönderdik bu sınıf entity den gelen bir değer olacak.
+    {
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetList();
+        T GetByID(int id);
+    }
+}
