abf03e755930ed8cc875ef0ccf426209574217d3
Melih Dinçer <m_dincer41@hotmail.com>
2023-02-27 20:04:02 +0300

    Default controller oluşturuldu.

---
+++ NetCore_Proje/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // Backend mimaride yazdığımız kodları burada çağırıp, UI (Kullanıcı arayüzü) ile birleştireceğiz.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
