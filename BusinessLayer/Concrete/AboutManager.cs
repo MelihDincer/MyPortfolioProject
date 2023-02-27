a8a1f503bbb813e464f4f9477c08a75a6c775d3a
Melih Dinçer <m_dincer41@hotmail.com>
2023-02-27 18:01:18 +0300

    AboutManger class ı eklendi.

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService // Bir interface miras alınıyorsa, o interface nin içerisindeki metodları bu class ın içerisine dahil etmemiz (implamente) gerekmektedir.
+    {
+        IAboutDal _aboutDal;
+
+        //Constructor oluşturuldu.
+        public AboutManager(IAboutDal aboutDal) 
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public List<About> GetList()
+        {
+            return _aboutDal.GetList();
+        }
+
+        public void TAdd(About t)
+        {
+            _aboutDal.Insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutDal.Delete(t);
+        }
+
+        public About TGetById(int id)
+        {
+            return _aboutDal.GetByID(id);
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.Update(t);
+        }
+    }
+}
