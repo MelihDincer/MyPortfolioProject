7873e52a2933e685ad805b7cd4c4b0ea9de44916
Melih Dinçer <m_dincer41@hotmail.com>
2023-03-02 22:03:27 +0300

    Portfolio tablosu silindi. (Bir problemden dolayı)

--- DataAccessLayer/Migrations/20230223190759_mig1.cs
+++ DataAccessLayer/Migrations/20230223190759_mig1.cs
@@ -90,19 +90,19 @@ namespace DataAccessLayer.Migrations
                     table.PrimaryKey("PK_Messages", x => x.MessageID);
                 });
 
-            migrationBuilder.CreateTable(
-                name: "Portfolios",
-                columns: table => new
-                {
-                    PortfolioID = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Portfolios", x => x.PortfolioID);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "Portfolios",
+            //    columns: table => new
+            //    {
+            //        PortfolioID = table.Column<int>(type: "int", nullable: false)
+            //            .Annotation("SqlServer:Identity", "1, 1"),
+            //        Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+            //        ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Portfolios", x => x.PortfolioID);
+            //    });
 
             migrationBuilder.CreateTable(
                 name: "Services",
