d32f6254c495b2bf8712716bd265108716a58816
Melih Dinçer <m_dincer41@hotmail.com>
2023-03-02 22:07:21 +0300

    Güncelleme yapıldı.

---
+++ NetCore_Proje/ViewComponents/Portfolio/PortfolioList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.ViewComponents.Portfolio
+{
+    public class PortfolioList : ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
