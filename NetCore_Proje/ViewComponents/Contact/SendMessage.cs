a478399a421821caf803c77152614485f1694efb
Melih Dinçer <m_dincer41@hotmail.com>
2023-03-03 20:52:34 +0300

    SendMessage ViewComponenti eklendi.

---
+++ NetCore_Proje/ViewComponents/Contact/SendMessage.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace NetCore_Proje.ViewComponents.Contact
+{
+    public class SendMessage : ViewComponent
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+
+        [HttpGet]
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IViewComponentResult Invoke(Message p)
+        //{
+        //    p.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString()); // Mesajın kaydedildiği andaki tarihi veritabanına yansıtma işlemi.
+        //    p.Status = true;
+        //    messageManager.TAdd(p);
+        //    return View();
+        //}
+    }
+}
