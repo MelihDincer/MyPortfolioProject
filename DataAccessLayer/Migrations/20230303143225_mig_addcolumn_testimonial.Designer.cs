be2cf9361a7f6f6aeb1d8458232ac70a8935c8b1
Melih Dinçer <m_dincer41@hotmail.com>
2023-03-03 19:00:03 +0300

    Testimonials tablosuna yeni bir sütun eklendi.

--- DataAccessLayer/Migrations/20230303143225_mig_addcolumn_testimonial.Designer.cs
+++ DataAccessLayer/Migrations/20230303143225_mig_addcolumn_testimonial.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230303143225_mig_addcolumn_testimonial")]
+    partial class mig_addcolumn_testimonial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
