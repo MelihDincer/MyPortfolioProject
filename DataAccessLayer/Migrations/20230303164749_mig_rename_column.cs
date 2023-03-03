560c3ca73f38c1023b7734b0ffee3f1a5b35b85a
Melih Dinçer <m_dincer41@hotmail.com>
2023-03-03 20:51:22 +0300

    Tablo ismindeki yanlışlık düzeltildi.

---
+++ DataAccessLayer/Migrations/20230303164749_mig_rename_column.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_rename_column : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contexts",
+                table: "Contexts");
+
+            migrationBuilder.RenameTable(
+                name: "Contexts",
+                newName: "Contacts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contacts",
+                table: "Contacts",
+                column: "ContactID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contacts",
+                table: "Contacts");
+
+            migrationBuilder.RenameTable(
+                name: "Contacts",
+                newName: "Contexts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contexts",
+                table: "Contexts",
+                column: "ContactID");
+        }
+    }
+}
