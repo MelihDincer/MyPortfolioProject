93654b7de78fb3bdbcbcd95d0277ff773911e3e7
Melih Dinçer <m_dincer41@hotmail.com>
2023-03-04 15:30:36 +0300

    Admin controller ı oluşturuldu.

---
+++ NetCore_Proje/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
