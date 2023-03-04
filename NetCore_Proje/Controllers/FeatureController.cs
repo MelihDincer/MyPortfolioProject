adf8702e6b448a0f24e0eda84e8cd9c40f688217
Melih Dinçer <m_dincer41@hotmail.com>
2023-03-04 15:31:11 +0300

    FeatureController oluşturuldu.

---
+++ NetCore_Proje/Controllers/FeatureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.Controllers
+{
+    public class FeatureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
