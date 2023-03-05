1d47731a85c2b7742f366991cc690fd1141f2df8
Melih Dinçer <m_dincer41@hotmail.com>
2023-03-05 16:35:33 +0300

    Yeni skill eklenebilmesi için HttpGet ve HttpPost olarak AdSkill IActionResult tanımlandı.

--- NetCore_Proje/Controllers/SkillController.cs
+++ NetCore_Proje/Controllers/SkillController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace NetCore_Proje.Controllers
@@ -11,6 +12,19 @@ namespace NetCore_Proje.Controllers
         {
             var values = skillManager.TGetList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill) 
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
         }
     }
 }
