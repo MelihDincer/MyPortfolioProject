c2ebdbd0828ff017a110116b8d70c0e45cbfe0c2
Melih Dinçer <m_dincer41@hotmail.com>
2023-03-05 15:20:40 +0300

    Yeteneklerin listelenmesi gerçekleştirildi.

--- NetCore_Proje/Controllers/SkillController.cs
+++ NetCore_Proje/Controllers/SkillController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NetCore_Proje.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
-            return View();
+            var values = skillManager.TGetList();
+            return View(values);
         }
     }
 }
