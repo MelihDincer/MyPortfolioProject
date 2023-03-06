9a33b954514c0b39ff931fa45a0a7679d5515be1
Melih Dinçer <m_dincer41@hotmail.com>
2023-03-06 16:02:10 +0300

    Viewbag ler tanımlandı. Yetenek silme ve güncelleme için gerekli ActionResult ların kodları yazıldı.

--- NetCore_Proje/Controllers/SkillController.cs
+++ NetCore_Proje/Controllers/SkillController.cs
@@ -10,6 +10,9 @@ namespace NetCore_Proje.Controllers
         SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            ViewBag.v1 = "Yetenek Listesi";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Listesi";
             var values = skillManager.TGetList();
             return View(values);
         }
@@ -17,13 +20,40 @@ namespace NetCore_Proje.Controllers
         [HttpGet]
         public IActionResult AddSkill()
         {
+            ViewBag.v1 = "Yetenek Ekleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Ekleme";
             return View();
         }
 
         [HttpPost]
-        public IActionResult AddSkill(Skill skill) 
+        public IActionResult AddSkill(Skill skill)
         {
             skillManager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteSkill(int id)
+        {
+            var values = skillManager.TGetById(id);
+            skillManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditSkill(int id)
+        {
+            ViewBag.v1 = "Yetenek Güncelleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Güncelleme";
+            var values = skillManager.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditSkill(Skill skill)
+        {
+            skillManager.TUpdate(skill);
             return RedirectToAction("Index");
         }
     }
