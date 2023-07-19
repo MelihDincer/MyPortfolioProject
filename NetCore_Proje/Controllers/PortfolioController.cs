9659450cd9a5ed45581a7e2c70e6f2aa7755dec9
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-19 12:14:17 +0300

    Listeleme, Ekleme, Silme, Güncelleme işlemleri tamamlandı.

---
+++ NetCore_Proje/Controllers/PortfolioController.cs
@@ -0,0 +1,83 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projelerim";
+            ViewBag.v3 = "Proje Listesi";
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            ViewBag.v1 = "Proje Ekleme";
+            ViewBag.v2 = "Projelerim";
+            ViewBag.v3 = "Proje Ekle";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio p)
+        {
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult results = validations.Validate(p); //validations isimli referans aracılığıyla parametreden gelen değerin geçerliliğini kontrol et.
+            if (results.IsValid) //results geçerli ise;
+            {
+                portfolioManager.TAdd(p); //ekleme işlemini gerçekleştir
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);//PortfolioValidator sınıfımızda ilgili property'e göre ilgili hata gelecek.
+                }
+            }
+            return View();
+        }
+        public IActionResult DeletePortfolio(int id)
+        {
+            var value = portfolioManager.TGetById(id);
+            portfolioManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditPortfolio(int id)
+        {
+            ViewBag.v1 = "Proje Düzenleme";
+            ViewBag.v2 = "Projelerim";
+            ViewBag.v3 = "Proje Düzenle";
+            var value = portfolioManager.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult EditPortfolio(Portfolio portfolio)
+        {
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult results = validations.Validate(portfolio);
+            if (results.IsValid)
+            {
+                portfolioManager.TUpdate(portfolio);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ NetCore_Proje/Controllers/ServiceController.cs
@@ -0,0 +1,61 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Hizmet Listesi";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmet Listesi";
+            var values = serviceManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            ViewBag.v1 = "Hizmet Ekleme";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmet Ekle";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            serviceManager.TAdd(service);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteService(int id)
+        {
+            var values = serviceManager.TGetById(id);
+            serviceManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            ViewBag.v1 = "Hizmet Güncelleme";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmet Güncelle";
+            var values = serviceManager.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditService(Service service)
+        {
+            serviceManager.TUpdate(service);
+            return RedirectToAction("Index");
+        }
+    }
+}
