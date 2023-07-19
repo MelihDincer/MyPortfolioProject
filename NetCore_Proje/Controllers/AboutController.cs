6921ec4bcdbd69479c7e8ae4446630d013a70bad
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-19 12:08:58 +0300

    AboutController oluşturuldu.

---
+++ NetCore_Proje/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Hakkımda";
+            ViewBag.v3 = "Hakkımda Sayfası";
+            var values = aboutManager.TGetById(1); //id değeri "1" olan verileri getir.
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult Index(About about)
+        {
+            aboutManager.TUpdate(about);
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
