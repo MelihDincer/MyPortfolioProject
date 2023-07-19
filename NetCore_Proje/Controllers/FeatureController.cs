138364f811e0112ee96d3c222513fff851c8332a
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-19 12:12:43 +0300

    Feature sayfası için listeleme ve güncelleme işlemleri tamamlandı.

--- NetCore_Proje/Controllers/FeatureController.cs
+++ NetCore_Proje/Controllers/FeatureController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NetCore_Proje.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Öne Çıkanlar";
+            ViewBag.v3 = "Öne Çıkanlar Sayfası";
+            var values = featureManager.TGetById(1);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult Index(Feature feature)
+        {
+            featureManager.TUpdate(feature);
+            return RedirectToAction("Index","Default");
         }
     }
 }
