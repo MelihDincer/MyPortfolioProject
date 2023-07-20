b2021d88c5fe2828ef4a870270fed9ebe31605ab
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-20 15:38:59 +0300

    ViewComponent ile FeatureStatistics bölümü yapıldı.

---
+++ NetCore_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace NetCore_Proje.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            Context c = new();
+            //var values = contactManager.TGetList();
+            ViewBag.v1 = c.Skills.Count();
+            ViewBag.v2 = c.Messages.Where(x => x.Status == false).Count(); //okunmamış mesaj sayısını getir.
+            ViewBag.v3 = c.Messages.Where(x => x.Status == true).Count(); //okunmuş mesaj sayısını getir.
+            ViewBag.v4 = c.Experiences.Count(); 
+            return View();
+        }
+    }
+}
