36fb4cb225c1fc8eadc200f49bae0cca06268277
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-20 15:47:10 +0300

    StatisticsDashboard2 ViewComponenti oluşturuldu.

---
+++ NetCore_Proje/ViewComponents/Dashboard/StatisticsDashboard2.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace NetCore_Proje.ViewComponents.Dashboard
+{
+    public class StatisticsDashboard2:ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Portfolios.Count(); //Proje sayısı
+            ViewBag.v2 = c.Messages.Count(); //Mesaj sayısı
+            ViewBag.v3 = c.Services.Count(); //Hizmet sayısı
+            return View();
+        }
+    }
+}
