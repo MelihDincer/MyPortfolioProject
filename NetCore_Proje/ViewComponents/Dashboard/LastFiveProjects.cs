31536bba1e37239cef173d2302e3045a4fe4e91d
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-20 15:39:45 +0300

    ViewComponent ile LastFiveProjects bölümü yapıldı. Son 5 proje listesi gösterildi.

---
+++ NetCore_Proje/ViewComponents/Dashboard/LastFiveProjects.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace NetCore_Proje.ViewComponents.Dashboard
+{
+    public class LastFiveProjects : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+            var values = portfolioManager.TGetList().OrderByDescending(x=>x.PortfolioID).Take(5).ToList();
+            return View(values);
+        }
+
+    }
+}
