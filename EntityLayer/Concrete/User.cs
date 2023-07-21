cecfb3cedf8a1a0c704b742dc9261f3ffc068c49
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-21 20:59:19 +0300

    User tablosu oluşturuldu.

---
+++ EntityLayer/Concrete/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class User
+    {
+        [Key]
+        public int UserID{ get; set; }
+        public string Name{ get; set; }
+        public string Surname{ get; set; }
+        public string Username{ get; set; }
+        public string Mail{ get; set; }
+        public string Password{ get; set; }
+        public string ImageURL{ get; set; }
+        public bool Status{ get; set; }
+        public List<UserMessage> UserMessages { get; set; }
+    }
+}
