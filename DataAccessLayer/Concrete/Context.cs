75c1a765727ef84155d823a14a726f4173410c19
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-21 20:59:10 +0300

    User ve UserMessage tabloları tanımlandı.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -27,5 +27,7 @@ namespace DataAccessLayer.Concrete
         public DbSet<Skill> Skills { get; set; }
         public DbSet<SocialMedia> SocialMedias { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserMessage> UserMessages { get; set; }
     }
 }
