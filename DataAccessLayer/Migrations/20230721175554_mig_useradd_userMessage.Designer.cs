d2e2c060fad290717e5348837bc4649080feae8d
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-21 20:58:46 +0300

    User ve UserMessage tabloları için migration uygulandı.

--- DataAccessLayer/Migrations/20230721175554_mig_useradd_userMessage.Designer.cs
+++ DataAccessLayer/Migrations/20230721175554_mig_useradd_userMessage.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230721175554_mig_useradd_userMessage")]
+    partial class mig_useradd_userMessage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
