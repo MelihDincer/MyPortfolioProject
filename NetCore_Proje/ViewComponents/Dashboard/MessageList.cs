492dbfc0288f17512642fbf0809dcdc66e912388
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-22 14:25:12 +0300

    MessageList ViewComponenti oluşturuldu.

--- NetCore_Proje/ViewComponents/Dashboard/MessageList.cs
+++ NetCore_Proje/ViewComponents/Dashboard/MessageList.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NetCore_Proje.ViewComponents.Dashboard
 {
     public class MessageList : ViewComponent
     {
+        UserMessageManager userMessageManager = new UserMessageManager(new EfUserMessageDal());
         public IViewComponentResult Invoke()
         {
-
-            return View();
+            var values = userMessageManager.GetUserMessageWithUserService();
+            return View(values);
         }
     }
 }
