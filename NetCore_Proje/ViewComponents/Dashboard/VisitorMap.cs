e51837186ede215101b6e3456e576db1958bdd04
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-22 14:29:33 +0300

    VisitorMap ViewComponent oluşturuldu.

---
+++ NetCore_Proje/ViewComponents/Dashboard/VisitorMap.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.ViewComponents.Dashboard
+{
+    public class VisitorMap : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
