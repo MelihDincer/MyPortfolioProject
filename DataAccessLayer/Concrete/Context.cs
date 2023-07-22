b8552e44d36670ad1adc4b5ce51cd68fad996539
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-22 14:32:13 +0300

    ToDoList tablosu Context'e eklendi.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -29,5 +29,6 @@ namespace DataAccessLayer.Concrete
         public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserMessage> UserMessages { get; set; }
+        public DbSet<ToDoList> ToDoLists { get; set; }
     }
 }
