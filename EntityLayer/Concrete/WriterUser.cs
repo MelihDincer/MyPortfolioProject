0bee060c84ced036c4988817e6d6b129f8f8815c
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-23 12:54:02 +0300

    Identity tabloları için IdentityUser'ı kullanan Writer user entitysi oluşturuldu. Bu entity de Name,Surname ve ImageURL bilgileri tutulacak.

---
+++ EntityLayer/Concrete/WriterUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class WriterUser : IdentityUser<int>
+    {
+        [Key]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
