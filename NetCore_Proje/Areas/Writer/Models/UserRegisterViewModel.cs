fc338c46746f98c88c6d1f7c50266f30b3113f40
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-23 13:03:49 +0300

    Kullanıcı kayıt paneli için ilgili property ler UserRegisterViewModel oluşturularak eklendi.

---
+++ NetCore_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCore_Proje.Areas.Writer.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Lütfen Adınızı Girin")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Lütfen Soyadınızı Girin")]
+        public string SurnameName { get; set; }
+        [Required(ErrorMessage = "Lütfen Resim URL Değeri Girin")]
+        public string ImageURL { get; set; }
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adını Girin")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Lütfen Şifreyi Girin")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Lütfen Şifreyi Tekrar Girin")]
+        [Compare("Password", ErrorMessage = "Şifreler Uyumlu Değil!")]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Lütfen Mail Girin")]
+        public string Mail { get; set; }
+    }
+}
