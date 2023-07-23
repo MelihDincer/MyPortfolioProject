a90599567509917f3c4368aa692a3676c7983e68
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-23 13:02:42 +0300

    Kullanıcıların kayıt olabileceği sayfanın yer aldığı RegisterController oluşturuldu.

---
+++ NetCore_Proje/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,56 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NetCore_Proje.Areas.Writer.Models;
+using System.Threading.Tasks;
+
+namespace NetCore_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class RegisterController : Controller
+    {
+
+        private readonly UserManager<WriterUser> _userManager;
+        
+        //Dependency Injection
+        public RegisterController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserRegisterViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                WriterUser w = new()
+                {
+                    Name = p.Name,
+                    Surname = p.SurnameName,
+                    Email = p.Mail,
+                    UserName = p.UserName,
+                    ImageURL = p.ImageURL
+                };
+                var result = await _userManager.CreateAsync(w,p.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
