eba174693008c6620989d18f0b2f282baee0c0b5
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-24 14:17:49 +0300

    Register Model sınıfına UserName property si eklendi.

--- NetCore_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
+++ NetCore_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -1,18 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace NetCore_Proje.Areas.Writer.Models
+namespace NetCore_Proje.Area.Writer.Models
 {
     public class UserRegisterViewModel
     {
         [Required(ErrorMessage = "Lütfen Adınızı Girin")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Lütfen Soyadınızı Girin")]
-        public string SurnameName { get; set; }
+        public string Surname { get; set; }
         [Required(ErrorMessage = "Lütfen Resim URL Değeri Girin")]
         public string ImageURL { get; set; }
-        [Required(ErrorMessage = "Lütfen Kullanıcı Adını Girin")]
+        [Required(ErrorMessage = "Lütfen bir ImageURL Girin")]
         public string UserName { get; set; }
-        [Required(ErrorMessage = "Lütfen Şifreyi Girin")]
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adını Girin")]
         public string Password { get; set; }
         [Required(ErrorMessage = "Lütfen Şifreyi Tekrar Girin")]
         [Compare("Password", ErrorMessage = "Şifreler Uyumlu Değil!")]
