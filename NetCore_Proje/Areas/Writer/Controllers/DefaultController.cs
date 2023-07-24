06045ae698242ce7301a8e2270c80029a52b358b
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-24 14:07:08 +0300

    Authentication işlemi için Authorize attribute eklendi.

--- NetCore_Proje/Areas/Writer/Controllers/DefaultController.cs
+++ NetCore_Proje/Areas/Writer/Controllers/DefaultController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-namespace NetCore_Proje.Areas.Writer.Controllers
+namespace NetCore_Proje.Area.Writer.Controllers
 {
     [Area("Writer")]
+    [Authorize]
     public class DefaultController : Controller
     {
         public IActionResult Index()
