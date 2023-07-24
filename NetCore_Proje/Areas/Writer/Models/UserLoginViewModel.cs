8c41d312b89b7b01ab24d446db490ba803af315c
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-24 14:18:06 +0300

    Login Model Sınıfı oluşturuldu.

---
+++ NetCore_Proje/Areas/Writer/Models/UserLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCore_Proje.Area.Writer.Models
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adını Giriniz...!")]
+        public string Username { get; set; }
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Lütfen Şifreyi Giriniz...!")]
+        public string Password { get; set; }
+    }
+}
