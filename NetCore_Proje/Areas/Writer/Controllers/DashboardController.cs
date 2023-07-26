e543589a7b390ba0fbc7b615b8c4de50d6d817d1
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-26 19:26:24 +0300

    User panelinde bulunan Dashboard bölümü için, Hava durumu api kullanılarak güncel hava durumu verileri apiden güncellenecek şekilde eklendi. Ayrıca dashboard a bazı istatistik verileriler de eklendi.

--- NetCore_Proje/Areas/Writer/Controllers/DashboardController.cs
+++ NetCore_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -1,13 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace NetCore_Proje.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + " " + values.Surname;
+
+            //Weather Api
+            string api = "3b53c407e37904ff6a9a7fffed8f8b50";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=kocaeli&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v5 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+            //statistics
+            Context c = new();
+            ViewBag.v1 = 0;
+            ViewBag.v2 = c.Announcements.Count();
+            ViewBag.v3 = 0;
+            ViewBag.v4 = c.Skills.Count();
             return View();
         }
     }
 }
+
+/*
+https://api.openweathermap.org/data/2.5/weather?q=kocaeli&mode=xml&lang=tr&units=metric&appid=3b53c407e37904ff6a9a7fffed8f8b50
+*/
