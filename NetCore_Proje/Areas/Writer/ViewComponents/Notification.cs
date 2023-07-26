83eae5ad2bd08c0ba08cdf46bfc2d07c1d2facdc
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-26 19:32:27 +0300

    User panelinde bildirimlerin listelenmesi ve erişimin kolay olması bakımından Notification ViewComponent i eklendi.

---
+++ NetCore_Proje/Areas/Writer/ViewComponents/Notification.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace NetCore_Proje.Areas.Writer.ViewComponents
+{
+    public class Notification : ViewComponent
+    {
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = announcementManager.TGetList().Take(5).ToList();
+            return View(values);
+        }
+    }
+}
