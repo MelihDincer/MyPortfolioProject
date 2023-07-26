1cfd5ca53bbf1e91c1dd822a16ae4f7f88f5e4c6
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-26 19:29:40 +0300

    Sistemde oturumu açık olan kullanıcının profil resminin sayfada görüntülenebilmesi için NavbarViewComponent  oluşturuldu.

---
+++ NetCore_Proje/Areas/Writer/ViewComponents/Navbar.cs
@@ -0,0 +1,35 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NetCore_Proje.Areas.Writer.ViewComponents
+{
+    public class Navbar : ViewComponent
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public Navbar(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // 2 farklı yol ile aynı sonuç alınabilmektedir.
+        // 1.YOL
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = user.ImageURL;
+            return View(user);
+        }
+
+        /* 2.YOL
+        public IViewComponentResult Invoke()
+        {
+            var user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+            ViewBag.v = user.ImageURL;
+            return View(user);
+        }
+        */
+    }
+}
