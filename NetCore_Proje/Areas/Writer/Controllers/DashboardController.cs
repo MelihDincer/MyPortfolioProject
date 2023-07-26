59c5f4a190ea656f3a07d7553ad03e6c0a96e90f
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-26 13:20:39 +0300

    User panelindeki Dashboard bölümü için DashboardController oluşturuldu.

---
+++ NetCore_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
