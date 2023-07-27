b23fed2a9149d0237bbc6c55e19f77c1194dbeb0
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-27 12:19:52 +0300

    WriterMessage Entity eklendi.

---
+++ EntityLayer/Concrete/WriterMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class WriterMessage
+    {
+        [Key]
+        public int WriterMessageID { get; set; }
+        public string Sender { get; set; } //Gönderen : mail olarak tutulacak
+        public string Receiver { get; set; } //Alıcı : mail olarak tutulacak
+        public string Subject { get; set; } //Konu
+        //public string Content { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
