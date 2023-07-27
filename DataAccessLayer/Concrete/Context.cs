0ef18f3194e8b962df96714398542502219c2dc1
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-27 12:16:21 +0300

    Mesajlar bölümü için WriterMessage tablosu Context sınıfımıza eklendi.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -32,5 +32,6 @@ namespace DataAccessLayer.Concrete
         public DbSet<UserMessage> UserMessages { get; set; }
         public DbSet<ToDoList> ToDoLists { get; set; }
         public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<WriterMessage> WriterMessages { get; set; }
     }
 }
