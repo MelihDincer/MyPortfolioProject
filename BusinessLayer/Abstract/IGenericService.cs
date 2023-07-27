65879b5aad766ce0edaf80c4c0d114febb376bce
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-27 12:13:18 +0300

    Mesajların şartlı listelenmesi için IGenericService da bir imza daha tanımlandı.

--- BusinessLayer/Abstract/IGenericService.cs
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -13,5 +13,7 @@ namespace BusinessLayer.Abstract
         void TUpdate(T t);
         List<T> TGetList();
         T TGetById(int id);
+        //Şartlı Listeleme
+        List<T> TGetListByFilter();
     }
 }
