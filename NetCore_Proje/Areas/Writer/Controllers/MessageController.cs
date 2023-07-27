f8306f9b2d5226c37693c405fbde399a2f6edd6f
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-27 12:18:36 +0300

    User panelindeki mesajlar için MessageController oluşturuldu.

---
+++ NetCore_Proje/Areas/Writer/Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NetCore_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class MessageController : Controller
+    {
+        WriterMessageManager _writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+
+        private readonly UserManager<WriterUser> _userManager;
+
+        public MessageController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index(string p)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            p = values.Email;
+            var messageList = _writerMessageManager.TGetListByFilter(p);
+            return View(messageList);
+        }
+    }
+}
