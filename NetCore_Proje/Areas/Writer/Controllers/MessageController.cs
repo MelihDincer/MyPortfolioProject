43ee1637567a7cc703500cbec344a20898e2c889
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-28 11:54:34 +0300

    Route attribute ile rotalar ayarlandı. Gönderilen-Alınan mesajlar sayfaları ve bunların detaylarını gördüğümüz sayfalar ile birlikte yeni mesaj oluşturma işlemi tamamlandı.

--- NetCore_Proje/Areas/Writer/Controllers/MessageController.cs
+++ NetCore_Proje/Areas/Writer/Controllers/MessageController.cs
@@ -1,16 +1,21 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace NetCore_Proje.Areas.Writer.Controllers
 {
     [Area("Writer")]
+    //[Route("Writer/[controller]")]
+    [Route("Writer/Message")]
     public class MessageController : Controller
     {
-        WriterMessageManager _writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
 
         private readonly UserManager<WriterUser> _userManager;
 
@@ -18,13 +23,59 @@ namespace NetCore_Proje.Areas.Writer.Controllers
         {
             _userManager = userManager;
         }
-
-        public async Task<IActionResult> Index(string p)
+        [Route("")]
+        [Route("RecieverMessage")]
+        public async Task<IActionResult> RecieverMessage(string p)
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             p = values.Email;
-            var messageList = _writerMessageManager.TGetListByFilter(p);
+            var messageList = writerMessageManager.GetListRecieverMessage(p);
             return View(messageList);
+        }
+        [Route("")]
+        [Route("SenderMessage")]
+        public async Task<IActionResult> SenderMessage(string p)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            p = values.Email;
+            var messageList = writerMessageManager.GetListSenderMessage(p);
+            return View(messageList);
+        }
+        [Route("SenderMessageDetails/{id}")]
+        public IActionResult SenderMessageDetails(int id)
+        {
+            WriterMessage writerMessage = writerMessageManager.TGetById(id);
+            return View(writerMessage);
+        }
+        [Route("RecieverMessageDetails/{id}")]
+        public IActionResult RecieverMessageDetails(int id)
+        {
+            WriterMessage writerMessage = writerMessageManager.TGetById(id);
+            return View(writerMessage);
+        }
+        [HttpGet]
+        [Route("")]
+        [Route("SendMessage")]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("")]
+        [Route("SendMessage")]
+        public async Task<IActionResult> SendMessage(WriterMessage p)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            string mail = values.Email;
+            string name = values.Name + " " + values.Surname;
+            p.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.Sender = mail;
+            p.SenderName = name;
+            Context c = new Context();
+            var usernamesurname = c.Users.Where(x => x.Email == p.Receiver).Select(y => y.Name + " " + y.Surname).FirstOrDefault();
+            p.ReceiverName = usernamesurname;
+            writerMessageManager.TAdd(p);
+            return RedirectToAction("SenderMessage");
         }
     }
 }
