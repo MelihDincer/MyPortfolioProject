b83d7abb8db15c0a34b7ec631af115d5fbe8b34c
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-28 11:55:48 +0300

    Route attribute ile rota düzenlemesi yapıldı. Kullanıcı şifre yenileme işlemi tamamlandı.

--- NetCore_Proje/Areas/Writer/Controllers/ProfileController.cs
+++ NetCore_Proje/Areas/Writer/Controllers/ProfileController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 namespace NetCore_Proje.Areas.Writer.Controllers
 {
     [Area("Writer")]
+    [Route("Writer/[controller]/[action]")]
     public class ProfileController : Controller
     {
         private readonly UserManager<WriterUser> _userManager;
@@ -45,10 +46,11 @@ namespace NetCore_Proje.Areas.Writer.Controllers
             }
             user.Name = p.Name;
             user.Surname = p.Surname;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, p.Password);
             var result = await _userManager.UpdateAsync(user);
             if(result.Succeeded)
             {
-                return RedirectToAction("Index","Default","Writer");
+                return RedirectToAction("Index","Login");
             }
             return View();
         }
