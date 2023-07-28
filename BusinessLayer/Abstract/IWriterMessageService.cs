0fc25cea4fa7c3265cccf9f119d4bc06e2fd63dd
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-28 11:45:02 +0300

    IWriterMessageService 'a özel gönderilen ve alınan mesajların listelenebilmesi için ilgili iki yeni metot imzası tanımlandı.

--- BusinessLayer/Abstract/IWriterMessageService.cs
+++ BusinessLayer/Abstract/IWriterMessageService.cs
@@ -9,5 +9,7 @@ namespace BusinessLayer.Abstract
 {
     public interface IWriterMessageService : IGenericService<WriterMessage>
     {
+        List<WriterMessage> GetListSenderMessage(string p);
+        List<WriterMessage> GetListRecieverMessage(string p);
     }
 }
