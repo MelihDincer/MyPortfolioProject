8d254db10a6ed9e98a0e38e036ef7ab9dcf78b19
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-28 11:50:46 +0300

    Route attribute ile rotalar ayarlandı.

--- NetCore_Proje/Areas/Writer/Controllers/DefaultController.cs
+++ NetCore_Proje/Areas/Writer/Controllers/DefaultController.cs
@@ -8,14 +8,16 @@ namespace NetCore_Proje.Area.Writer.Controllers
 {
     [Area("Writer")]
     [Authorize]
+    [Route("Writer/[controller]/[action]")]
     public class DefaultController : Controller
     {
-        AnnouncementManager announcementManager = new(new EfAnnouncementDal());
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
         public IActionResult Index()
         {
             var values = announcementManager.TGetList();
             return View(values);
         }
+        [HttpGet]
         public IActionResult AnnouncementDetails(int id)
         {
             var value = announcementManager.TGetById(id);
