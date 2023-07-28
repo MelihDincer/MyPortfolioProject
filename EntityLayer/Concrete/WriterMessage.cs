640f7d4528ddcbdbb7dc3efbd2a2ff8a0aea6e9e
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-28 11:48:02 +0300

    WriterMessage entity mizde Gönderici ve Alıcı adlarının tutulduğu iki property tanımlandı.

--- EntityLayer/Concrete/WriterMessage.cs
+++ EntityLayer/Concrete/WriterMessage.cs
@@ -13,6 +13,8 @@ namespace EntityLayer.Concrete
         public int WriterMessageID { get; set; }
         public string Sender { get; set; } //Gönderen : mail olarak tutulacak
         public string Receiver { get; set; } //Alıcı : mail olarak tutulacak
+        public string SenderName{ get; set; }
+        public string ReceiverName{ get; set; }
         public string Subject { get; set; } //Konu
         //public string Content { get; set; }
         public string MessageContent { get; set; }
