48adf25e16eb7195ac940d77987c5fde783b01db
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-29 16:26:25 +0300

    Projenin Ana controller ve sayfası olan "Default" sayfasından Admin'e mesaj gönderme işlemi için ContactController oluşturuldu, listeleme-silme-detay sayfalarının action kodları yazıldı.

---
+++ NetCore_Proje/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.Controllers
+{
+    //ADMIN'E GELEN MESAJLAR
+    public class ContactController : Controller
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Mesaj Listesi";
+            ViewBag.v2 = "Mesajlar";
+            ViewBag.v3 = "Mesaj Listesi Sayfası";
+            var values = messageManager.TGetList();
+            return View(values);
+        }
+        public IActionResult DeleteContact(int id)
+        {
+            var values = messageManager.TGetById(id);
+            messageManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult ContactDetails(int id) 
+        {
+            ViewBag.v1 = "Mesaj Detay";
+            ViewBag.v2 = "Mesajlar";
+            ViewBag.v3 = "Mesaj Detay Sayfası";
+            var values = messageManager.TGetById(id);
+            return View(values);
+        }
+    }
+}
