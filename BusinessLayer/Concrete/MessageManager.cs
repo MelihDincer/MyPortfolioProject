60373cfed88f4db4f70aba807d72d55b944446e5
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-29 16:14:52 +0300

    MessageManager'daki TDelete, TGetById ve TGetList metotlarının iş kodları yazıldı.

--- BusinessLayer/Concrete/MessageManager.cs
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -25,17 +25,17 @@ namespace BusinessLayer.Concrete
 
         public void TDelete(Message t)
         {
-            throw new NotImplementedException();
+            _messageDal.Delete(t);
         }
 
         public Message TGetById(int id)
         {
-            throw new NotImplementedException();
+            return _messageDal.GetByID(id);
         }
 
         public List<Message> TGetList()
         {
-            throw new NotImplementedException();
+            return _messageDal.GetList();
         }
 
         public List<Message> TGetListByFilter()
