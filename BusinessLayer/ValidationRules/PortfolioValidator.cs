6a47f87489788dbc8cfcf4fc196c68e60ac0eb58
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-29 16:15:10 +0300

    Portfolio validasyonları düzenlendi.

--- BusinessLayer/ValidationRules/PortfolioValidator.cs
+++ BusinessLayer/ValidationRules/PortfolioValidator.cs
@@ -12,13 +12,15 @@ namespace BusinessLayer.ValidationRules
     {
         public PortfolioValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().WithMessage("Proje adı boş geçilemez");
-            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Görsel alanı boş geçilemez");
-            RuleFor(x => x.ImageUrl2).NotEmpty().WithMessage("Görsel2 alanı boş geçilemez");
-            RuleFor(x => x.Price).NotEmpty().WithMessage("Fiyat alanı boş geçilemez");
-            RuleFor(x => x.Value).NotEmpty().WithMessage("Değer alanı boş geçilemez");
-            RuleFor(x => x.Name).MinimumLength(5).WithMessage("Proje adı en az 5 karakterden oluşmak zorundadır");
-            RuleFor(x => x.Name).MaximumLength(100).WithMessage("Proje adı en fazla 100 karakterden oluşmak zorundadır");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Proje adı boş geçilemez.");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Görsel-1 alanı boş geçilemez.");
+            RuleFor(x => x.ImageUrl2).NotEmpty().WithMessage("Görsel-2 alanı boş geçilemez.");
+            RuleFor(x => x.ProjectUrl).NotEmpty().WithMessage("Proje URL alanı boş geçilemez.");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Fiyat alanı boş geçilemez.");
+            RuleFor(x => x.Value).NotEmpty().WithMessage("Değer alanı boş geçilemez.");
+            RuleFor(x => x.Name).MinimumLength(5).WithMessage("Proje adı en az 5 karakterden oluşmak zorundadır.");
+            RuleFor(x => x.Name).MaximumLength(100).WithMessage("Proje adı en fazla 100 karakterden oluşmak zorundadır.");
+            RuleFor(x => x.Platform).NotEmpty().WithMessage("Platform için ilgili görseli yüklemelisiniz.");
         }
     }
 }
