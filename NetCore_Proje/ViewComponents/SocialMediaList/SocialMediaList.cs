8a22e7b051084025355a1b499c76cf7b5e3ad3f5
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-30 20:44:06 +0300

    ViewComponent oluşturuldu.

---
+++ NetCore_Proje/ViewComponents/SocialMediaList/SocialMediaList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.ViewComponents.SocialMediaList
+{
+    public class SocialMediaList : ViewComponent
+    {
+        SocialMediaManager socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = socialMediaManager.TGetList();
+            return View(values);
+        }
+    }
+}
