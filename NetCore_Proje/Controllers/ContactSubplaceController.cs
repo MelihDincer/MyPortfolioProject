cdf1a89d1ee32637c2fd6fb8ee6061dd675ffc3b
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-30 20:24:21 +0300

    Default sayfasındaki alt iletişim bölümü dinamik şekilde listelenmesi ve düzenlenmesi işlemleri tamamlandı.

---
+++ NetCore_Proje/Controllers/ContactSubplaceController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.Controllers
+{
+    public class ContactSubplaceController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "İletişim Bilgileri";
+            ViewBag.v2 = "İletişim Bilgileri";
+            ViewBag.v3 = "İletişim Bilgileri Düzenleme Sayfası";
+            var values = contactManager.TGetById(1); //id değeri "1" olan verileri getir.
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.ContactID = 1;
+            contactManager.TUpdate(contact);
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
