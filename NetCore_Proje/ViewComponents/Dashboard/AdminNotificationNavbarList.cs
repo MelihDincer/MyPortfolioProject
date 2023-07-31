c2d05fef6fde0f06ca5a88e2e229dc6694aa5089
Melih Dinçer <m_dincer41@hotmail.com>
2023-07-31 16:13:18 +0300

    Admin panelinde üst kısımda bulunan bildirimler bölümü için ViewComponent oluşturuldu.

---
+++ NetCore_Proje/ViewComponents/Dashboard/AdminNotificationNavbarList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.ViewComponents.Dashboard
+{
+    public class AdminNotificationNavbarList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
