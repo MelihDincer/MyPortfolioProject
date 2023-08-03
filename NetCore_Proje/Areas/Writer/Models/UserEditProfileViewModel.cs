84845ee9af2a2145b93ee18af3ea1bcca6d7ecfe
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-03 22:50:26 +0300

    UsereEditViewModel, şifre ve diğer profil bilgilerinin ayrı güncellenmesi gerektiğinden dolayı daha anlaşılır bir model adı belirlenip, şifre property leri silinip username ve mail property leri eklendi.

--- NetCore_Proje/Areas/Writer/Models/UserEditProfileViewModel.cs
+++ NetCore_Proje/Areas/Writer/Models/UserEditProfileViewModel.cs
@@ -2,12 +2,12 @@
 
 namespace NetCore_Proje.Areas.Writer.Models
 {
-    public class UserEditViewModel
+    public class UserEditProfileViewModel
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string Password { get; set; }
-        public string PasswordConfirm { get; set; }
+        public string Username { get; set; }
+        public string Mail { get; set; }
         public string PictureURL { get; set; }
         public IFormFile Picture { get; set; }
     }
