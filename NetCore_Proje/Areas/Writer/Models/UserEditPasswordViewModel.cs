56b7fca09a38dbbd390fecceedf1429b1d2a7b2a
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-03 22:51:16 +0300

    UsereEditViewModel 'dan ayrılan şifre property leri ayrı bir viewmodel olarak UserEditPasswordViewModel de tanımlandı.

---
+++ NetCore_Proje/Areas/Writer/Models/UserEditPasswordViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NetCore_Proje.Areas.Writer.Models
+{
+    public class UserEditPasswordViewModel
+    {
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+    }
+}
