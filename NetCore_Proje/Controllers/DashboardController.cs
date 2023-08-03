348694439d2e307b0f9924af28f593d539ca9e10
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-03 14:41:03 +0300

    Sadece admin yetkisi olan kullanıcıların dashboard'a erişebilmesi sağlandı.

--- NetCore_Proje/Controllers/DashboardController.cs
+++ NetCore_Proje/Controllers/DashboardController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NetCore_Proje.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class DashboardController : Controller
     {
         public IActionResult Index()
