9e92a15c8a6b430281aa47a3802ec4fdeec71343
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-04 13:04:29 +0300

    SideBar'da en üstte bulunan giriş yapan kullanıcının ad soyad ve resim bilgileri dinamik olarak getirileceği için, viewcomponent sınıfı olarak eklendi.

---
+++ NetCore_Proje/ViewComponents/Dashboard/NewSideBar.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NetCore_Proje.ViewComponents.Dashboard
+{
+    public class NewSideBar : ViewComponent
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public NewSideBar(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v1 = user.Name + " " + user.Surname;
+            ViewBag.v2 = user.ImageURL;
+            return View();
+        }
+    }
+}
