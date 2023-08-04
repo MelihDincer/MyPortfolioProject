60098eb2ea584f5f3f023a4aec8678d4f8ccd16d
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-04 13:09:39 +0300

    Dashboard sayfasında bulunan mesaj bölümü için component sayfası revize edildi.

--- NetCore_Proje/ViewComponents/Dashboard/MessageList.cs
+++ NetCore_Proje/ViewComponents/Dashboard/MessageList.cs
@@ -1,14 +1,30 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NetCore_Proje.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace NetCore_Proje.ViewComponents.Dashboard
 {
     public class MessageList : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        private readonly UserManager<WriterUser> _userManager;
+
+        public MessageList(UserManager<WriterUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v1 = user.ImageURL;
+            var values = writerMessageManager.TGetList().Where(x=>x.Receiver == "admin@gmail.com").Take(5).ToList();
+            return View(values);
         }
     }
 }
