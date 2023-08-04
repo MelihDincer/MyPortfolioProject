7fca9bb45f96a30dfc2cbcc4f8f5ab93c5a9b097
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-04 13:01:16 +0300

    AdminAccountImageNavbar viewcomponent sınıfı eklendi. Giriş yapan kullanıcının ad soyad ve resim bilgileri dinamik olarak belirlendi.

---
+++ NetCore_Proje/ViewComponents/Dashboard/AdminAccountImageNavbar.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NetCore_Proje.ViewComponents.Dashboard
+{
+    public class AdminAccountImageNavbar : ViewComponent
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public AdminAccountImageNavbar(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v1 = user.ImageURL;
+            ViewBag.v2 = user.Name + " " + user.Surname;
+            return View(user);
+        }
+    }
+}
