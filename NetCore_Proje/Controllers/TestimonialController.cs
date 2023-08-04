0f758c87f6637689467d4d58b14dcb42de2287a3
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-04 12:59:43 +0300

    Referans ekleme işlemleri yapıldı.

--- NetCore_Proje/Controllers/TestimonialController.cs
+++ NetCore_Proje/Controllers/TestimonialController.cs
@@ -16,6 +16,20 @@ namespace NetCore_Proje.Controllers
             var values = testimonialManager.TGetList();
             return View(values);
         }
+        [HttpGet]
+        public IActionResult AddTestimonial()
+        {
+            ViewBag.v1 = "Referanslar";
+            ViewBag.v2 = "Referanslar";
+            ViewBag.v3 = "Referans Ekleme Sayfası";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddTestimonial(Testimonial testimonial)
+        {
+            testimonialManager.TAdd(testimonial);
+            return RedirectToAction("Index");
+        }
         public IActionResult DeleteTestimonial(int id)
         {
             var values = testimonialManager.TGetById(id);
