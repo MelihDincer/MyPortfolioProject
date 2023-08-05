308ba3b7189e1cf6fee38cfc79150f0af516239a
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-05 15:13:02 +0300

    Mesaj gönderen kişilerin resimlerinin farklı tabloda olmasından dolayı ortak yeni bir viewmodel eklendi.

---
+++ NetCore_Proje/Models/MessageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using System;
+
+namespace NetCore_Proje.Models
+{
+    public class MessageViewModel
+    {
+        public int MessageID { get; set; }
+        public WriterMessage Message { get; set; }
+        public string SenderImage { get; set; }
+        public string SenderName { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
