48be514100185abaeb785936d5a7d888d4a1f77e
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-05 15:12:34 +0300

    Ajax ile yapılan deneyim sayfası revize edildi.

--- NetCore_Proje/Controllers/Experience2Controller.cs
+++ NetCore_Proje/Controllers/Experience2Controller.cs
@@ -1,20 +1,26 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace NetCore_Proje.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class Experience2Controller : Controller
     {
         ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
         public IActionResult Index()
         {
+            ViewBag.v1 = "Deneyim Listesi";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyimler Sayfası";
             return View();
         }
         public IActionResult ListExperience()
@@ -29,9 +35,9 @@ namespace NetCore_Proje.Controllers
             var values = JsonConvert.SerializeObject(p);
             return Json(values);
         }
-        public IActionResult GetById(int ExprerienceID)
+        public IActionResult GetById(int ExperienceID)
         {
-            var v = experienceManager.TGetById(ExprerienceID);
+            var v = experienceManager.TGetById(ExperienceID);
             var values = JsonConvert.SerializeObject(v);
             return Json(values);
         }
@@ -39,14 +45,20 @@ namespace NetCore_Proje.Controllers
         {
             var v = experienceManager.TGetById(id);
             experienceManager.TDelete(v);
-            return NoContent();
+            return Ok();
         }
-        public IActionResult UpdateExperince(Experience p)
+        public IActionResult UpdateExperience(int id, string name, string date)
         {
-            var v = experienceManager.TGetById(p.ExperienceID);
-            experienceManager.TUpdate(v);
-            var values = JsonConvert.SerializeObject(p);
-            return Json(values);
+            var v = experienceManager.TGetById(id);
+            if (v != null)
+            {
+                v.Name = name;
+                v.Date = date;
+                experienceManager.TUpdate(v);
+                var value = JsonConvert.SerializeObject(v);
+                return Json(value);
+            }
+            return NoContent();
         }
     }
 }
