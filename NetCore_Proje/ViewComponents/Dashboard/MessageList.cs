6c1245512af5e97d50211c16fb08cff98b05df85
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-05 15:14:36 +0300

    Dashboard'da iletişime geçen kişilerin mesajları yerine sisteme kayıtlı kullanıcıların göndermiş olduğu mesajların listelenmesi sağlandı.

--- NetCore_Proje/ViewComponents/Dashboard/MessageList.cs
+++ NetCore_Proje/ViewComponents/Dashboard/MessageList.cs
@@ -12,19 +12,38 @@ namespace NetCore_Proje.ViewComponents.Dashboard
 {
     public class MessageList : ViewComponent
     {
-        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
-        private readonly UserManager<WriterUser> _userManager;
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            string p = "admin@gmail.com";
+            var list = (from x in c.Users
+                        join y in c.WriterMessages on x.Email equals y.Sender
+                        where y.Receiver == p
+                        orderby y.WriterMessageID descending
+                        select new MessageViewModel
+                        {
+                            MessageID = y.WriterMessageID,
+                            Message = y,
+                            SenderImage = x.ImageURL,
+                            SenderName = y.SenderName,
+                            Date = y.Date
+                        }).Take(6).ToList();
+            return View(list);
+        }
+        //WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        //private readonly UserManager<WriterUser> _userManager;
 
-        public MessageList(UserManager<WriterUser> userManager)
-        {
-            _userManager = userManager;
-        }
-        public async Task<IViewComponentResult> InvokeAsync()
-        {
-            var user = await _userManager.FindByNameAsync(User.Identity.Name);
-            ViewBag.v1 = user.ImageURL;
-            var values = writerMessageManager.TGetList().Where(x=>x.Receiver == "admin@gmail.com").Take(5).ToList();
-            return View(values);
-        }
+        //public MessageList(UserManager<WriterUser> userManager)
+        //{
+        //    _userManager = userManager;
+        //}
+        //public async Task<IViewComponentResult> InvokeAsync()
+        //{
+        //    var user = await _userManager.FindByNameAsync(User.Identity.Name);
+        //    ViewBag.v1 = user.ImageURL;
+        //    var values = writerMessageManager.TGetList().Where(x=>x.Receiver == "admin@gmail.com").Take(5).ToList();
+        //    return View(values);
+        //}
+
     }
 }
