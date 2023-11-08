203e7e3ca59c60f8d631679cb7aca5e8d5730583
Melih DİNÇER <m_dincer41@hotmail.com>
2023-11-08 23:23:11 +0300

    Login işlemi sırasında gerçekleştirilen şartın kaldırılması.

--- NetCore_Proje/Areas/Writer/Controllers/LoginController.cs
+++ NetCore_Proje/Areas/Writer/Controllers/LoginController.cs
@@ -15,10 +15,13 @@ namespace NetCore_Proje.Area.Writer.Controllers
         //Dependency Injection Uygulaması --> Başlangıç
         private readonly SignInManager<WriterUser> _signInManager;
         public LoginController(SignInManager<WriterUser> signInManager)
-        //Dependency Injection Uygulaması --> Son
+
         {
             _signInManager = signInManager;
         }
+
+        //Dependency Injection Uygulaması --> Son
+
         //Kullanıcı Adı: admin41 <----> Şifre: 123456Aa*
 
         [HttpGet]
@@ -34,14 +37,7 @@ namespace NetCore_Proje.Area.Writer.Controllers
                 var result = await _signInManager.PasswordSignInAsync(p.Username, p.Password, true, true);
                 if (result.Succeeded)
                 {
-                    if(p.Username == "admin41")
-                    {
-                        return RedirectToAction("Index", "Dashboard");
-                    }
-                    else
-                    {
-                        return RedirectToAction("Index", "Profile", new { area = "Writer" });
-                    }
+                    return RedirectToAction("Index", "Dashboard", new { area = "Writer" });
                 }
                 else
                 {
