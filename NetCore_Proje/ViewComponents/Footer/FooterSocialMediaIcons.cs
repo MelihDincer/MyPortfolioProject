6274777b89d148f69db1205d487e3d0d1e1c8d42
Melih DİNÇER <m_dincer41@hotmail.com>
2023-11-09 23:31:58 +0300

    Footer kısmında bulunan sosyal medya ikonları için viewcomponent class ve partialview oluşturuldu.

---
+++ NetCore_Proje/ViewComponents/Footer/FooterSocialMediaIcons.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Proje.ViewComponents.Footer
+{
+    public class FooterSocialMediaIcons : ViewComponent
+    {
+        SocialMediaManager socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = socialMediaManager.TGetList();
+            return View(values);
+        }
+    }
+}
