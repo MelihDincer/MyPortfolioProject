258a7c600f4671840f230e731e122a2d938e2d19
Melih DİNÇER <m_dincer41@hotmail.com>
2023-11-11 15:01:13 +0300

    Writer Dashboard panelinde istatistik bölümü eklendi.

---
+++ NetCore_Proje/Areas/Writer/ViewComponents/Dashboard/Statistics.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace NetCore_Proje.Areas.Writer.ViewComponents.Dashboard
+{
+    public class Statistics : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.projectCount = c.Portfolios.Count();
+            ViewBag.skillCount = c.Skills.Count();
+            ViewBag.experienceCount = c.Experiences.Count();
+            ViewBag.userCount = c.Users.Count();
+            return View();
+        }
+    }
+}
