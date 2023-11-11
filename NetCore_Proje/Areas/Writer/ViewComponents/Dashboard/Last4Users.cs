89be1755103e550e041bc9b13f2429e1b823e570
Melih DİNÇER <m_dincer41@hotmail.com>
2023-11-11 15:00:40 +0300

    Writer Dashboard panelinde son 4 kullanıcı listesi bölümü eklendi.

---
+++ NetCore_Proje/Areas/Writer/ViewComponents/Dashboard/Last4Users.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace NetCore_Proje.Areas.Writer.ViewComponents.Dashboard
+{
+    public class Last4Users : ViewComponent
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public Last4Users(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _userManager.Users.OrderByDescending(x => x.Id).Take(4).ToList();
+            return View(values);
+        }
+    }
+}
