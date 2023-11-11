55eafba7d3a416b82d550ab24d9736ac5dc729b5
Melih DİNÇER <m_dincer41@hotmail.com>
2023-11-11 15:00:53 +0300

    Writer Dashboard panelinde son 5 proje listesi bölümü eklendi.

---
+++ NetCore_Proje/Areas/Writer/ViewComponents/Dashboard/Last5Projects.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace NetCore_Proje.Areas.Writer.ViewComponents.Dashboard
+{
+    public class Last5Projects : ViewComponent
+    {
+        private readonly IPortfolioService _portfolioService;
+
+        public Last5Projects(IPortfolioService portfolioService)
+        {
+            _portfolioService = portfolioService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _portfolioService.TGetList().OrderByDescending(x => x.PortfolioID).Take(5).ToList();
+            return View(values);
+        }
+    }
+}
